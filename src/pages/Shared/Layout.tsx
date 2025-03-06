f075ba127ce4c1eb1f5627cf459ffaa77c20032a
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-03-06 15:50:08 -0300

    Added floating icons menu

--- src/pages/Shared/Layout.tsx
+++ src/pages/Shared/Layout.tsx
@@ -2,6 +2,7 @@ import { ParentComponent } from "solid-js";
 import "./Layout.scss";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import { useTheme } from "../../context/ThemeContextProvider";
+import FloatingIcons from "../../components/FloatingIcons/FloatingIcons";
 
 const Layout: ParentComponent = (props) => {
   const { theme } = useTheme();
@@ -9,6 +10,7 @@ const Layout: ParentComponent = (props) => {
   return (
     <div class={`page-layout page-layout--${theme()}`}>
       <NavigationBar />
+      <FloatingIcons />
       {props.children}
     </div>
   );
