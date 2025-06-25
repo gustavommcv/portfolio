79dff6f2ec8f066bfe80b470f99083bd28fa0ad5
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-25 17:13:55 -0300

    change about description

--- src/pages/AboutPage/aboutTranslations.ts
+++ src/pages/AboutPage/aboutTranslations.ts
@@ -3,20 +3,20 @@ export const aboutTranslations = {
     introduction:
       "I'm Gustavo Monnerat, a software developer and a postgraduate student in Software Architecture.",
     experience:
-      "I work as a full-stack web developer with a strong foundation in technologies like Node.js, React, SQL, and MongoDB.",
+      "I work as a full-stack freelancer, developing projects with technologies such as Gin, Express.js (Node), React, Angular, Docker, and AWS.",
     approach:
-      "My journey is driven by a continuous pursuit of learning and technical improvement, blending theory and practice to create efficient and scalable solutions.",
+      "I'm always eager to learn more and contribute to relevant projects, whether in open-source or private solutions, with the goal of building well-structured and efficient systems.",
     focus:
-      "My focus is on developing modern, well-structured applications, ensuring code quality, best practices, and robust architecture."
+      "My focus is on developing modern, well-structured applications while ensuring code quality, best practices, and robust architecture. Currently, I'm concentrating on Go (Golang) in my studies.",
   },
   pt: {
     introduction:
       "Sou Gustavo Monnerat, desenvolvedor de software e pós-graduando em Arquitetura de Software.",
     experience:
-      "Atuo como desenvolvedor web full-stack, com conhecimento sólido em tecnologias como Node.js, React, SQL e MongoDB.",
+      "Atuo como freelancer full-stack, desenvolvendo projetos com tecnologias como Gin, Express.js (Node), React, Angular, Docker e AWS.",
     approach:
-      "Minha trajetória é marcada pela busca constante por aprendizado e aprimoramento técnico, combinando teoria e prática para criar soluções eficientes e escaláveis.",
+      "Minha trajetória é marcada pela busca constante por aprendizado e aprimoramento técnico, com o objetivo de criar soluções eficientes e escaláveis, combinando teoria e prática.",
     focus:
-      "Meu foco é desenvolver aplicações modernas e bem estruturadas, com atenção à qualidade do código, boas práticas e arquitetura robusta.",
+      "Meu foco é desenvolver aplicações modernas e bem estruturadas, garantindo qualidade de código, boas práticas e arquitetura robusta. Atualmente, estou focando em Go (Golang) em meus estudos.",
   },
 };
