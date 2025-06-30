e1a5cf10b3138d620dcc9b27328dc33804cb7af8
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-30 02:57:06 -0300

    simple contact section implementation

--- src/app/features/home/components/ContactSection/components/ContactLinks/ContactLinks.tsx
+++ src/app/features/home/components/ContactSection/components/ContactLinks/ContactLinks.tsx
@@ -1,15 +1,32 @@
-import CodeSnippet from "../../../../../../core/shared/CodeSnippet/CodeSnippet";
 import { useTheme } from "../../../../../contexts/ThemeContext/ThemeContextProvider";
 import "./ContactLinks.scss";
 
 // TODO
 // Copy & send email buttons
+// Copy number button
 export default function ContactLinks() {
   const { theme } = useTheme();
 
   return (
     <ul class={`contact-links contact-links--${theme()}`}>
-      <CodeSnippet />
+      <li>
+        <span class={`contact-links__span contact-links__span--${theme()}`}>
+          Email:
+        </span>{" "}
+        monnerat.gustavo@outlook.com
+      </li>
+      <li>
+        <span class={`contact-links__span contact-links__span--${theme()}`}>
+          Phone:
+        </span>{" "}
+        +55 (41)99189-2192
+      </li>
+      <li>
+        <span class={`contact-links__span contact-links__span--${theme()}`}>
+          Discord:
+        </span>{" "}
+        gustamonnerat
+      </li>
     </ul>
   );
 }
